4b8fb521c1c9567235e232a429819348a1c0dc87
Dyana Butler <dyanajbutler98@gmail.com>
2024-05-05 16:47:07 -0400

    it looks so good hell yeah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,27 @@
 import Image from "next/image"
 import Link from "next/link"
+import Button from "./components/Button.tsx"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-950 text-gray-50">
+    <div className="flex flex-col items-center justify-center h-screen text-gray-50">
       <div className="space-y-6 text-center">
-        <div className="text-3xl font-bold animate-pulse">Soon, the eyes see all</div>
-        <Image
-          alt="Animated GIF"
-          className="rounded-lg"
-          height={300}
-          src="/placeholder.svg"
-          style={{
-            aspectRatio: "300/300",
-            objectFit: "cover",
-          }}
-          width={300}
-        />
-        <Link
-          className="inline-flex items-center justify-center rounded-md bg-gray-800 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-700 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
-          href="#"
-        >
-          Join the Discord
-        </Link>
+        <div className="animate-text bg-gradient-to-r from-indigo-400 via-black to-indigo-400 bg-clip-text text-transparent text-xl font-king shadow-2xl ">Soon, the eyes see all</div>
+        <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+          <Image
+            alt="Animated GIF"
+            className="rounded-lg"
+            height={300}
+            src="/eye.gif"
+            style={{
+              aspectRatio: "300/300",
+              objectFit: "cover",
+            }}
+            width={300}
+          />
+        </div>
+        
+        <Button link="https://discord.gg/5yydr8Hd" label="Discord Invite Link" actionLabel="Become an OG in the Discord " />
       </div>
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,48 @@ const config: Config = {
   ],
   theme: {
     extend: {
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+        chop: ['var(--font-chop)'],
+        cloister: ['var(--font-cloister)'],
+        king: ['var(--font-king)'],
+      },
+      animation: {
+        tilt: 'tilt 10s infinite linear',
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+        tilt: {
+          '0%, 50%, 100%': {
+            transform: 'rotate(0deg)',
+          },
+          '25%': {
+            transform: 'rotate(1deg)',
+          },
+          '75%': {
+            transform: 'rotate(-1.5deg)',
+          },
+        },
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
